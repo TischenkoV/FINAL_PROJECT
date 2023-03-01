8f64dbc03c338fc11b4861e74c18f3d339d9bdbe
V.Tischenko <smart.solutions.rnd@ya.ru>
2023-03-01 14:42:05 +0300

    обновили основную программу

--- choice_specialization/Program.cs
+++ choice_specialization/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Создаем необходимые массивы
+
+string[] startArray = { "main", "you", "'ll", "world", " IT", "!" };
+string[] endArray = new string[startArray.Length];
+
+// Создаем вспомогательные переменные
+
+int count = startArray.Length;
+int index = 0;
+int lenEndArray = 0;
+
+// Основной цикл перебора исходного массива
+
+while (index < count)
+{
+    if (startArray[index].Length <= 3)
+    {
+        endArray[lenEndArray] = startArray[index];
+        lenEndArray++;
+        //Console.WriteLine(endArray[0]);
+    }
+    index++;
+}
+
+// Выводим в консоль результирующий массив
+
+PrintArray(endArray);
+
+
+////////////////////////////////////////////////////////
+void PrintArray(string[] col) // метод печати массива в строчку все элементы
+{
+    int count = col.Length;
+    int pos = 0;
+    while (pos < count)
+    {
+        Console.Write(col[pos]);
+        pos++;
+    }
+}
